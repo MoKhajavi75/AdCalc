e0987514edafb46afb80f87a381c8c9ad22a0548
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-02 13:50:45 +0330

    Add StyleSheet

---
+++ src/styles.js
@@ -0,0 +1,27 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+  
+    bala: {
+      flex: 3,
+      fontSize: 96,
+      color: 'red',
+      textAlign: 'center',
+      margin: 10,
+      backgroundColor: '#FF0000'
+    },
+  
+    paiin: {
+      flex: 7,
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+      backgroundColor: '#00FF00',
+    },
+  });
+
+export default styles;
