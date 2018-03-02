926d4b9c17102906f6b44adb946dd1d310591f74
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-03 01:07:03 +0330

    Add 3 Screens
    Title need to be Centered

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 // ********************
 
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, Dimensions } from 'react-native';
 import { StackNavigator } from 'react-navigation'
 
 import styles from './src/styles.js';
@@ -16,78 +16,79 @@ export default class AdCalc extends React.Component {
 }
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerTintColor: '#F0FFFF',
+
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+
+    headerRight: (<View><Text style = {{marginLeft: 120}}>Test</Text></View>),
+
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      marginStart: (Dimensions.get('window').width) / 2,
+    },
+  };
+
+  
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
-          <Button
-            title = "Go to Details"
-            onPress = {() => this.props.navigation.navigate('Details')}
-          />
-          <Button
-            title = "Go to My Page 😁"
-            color = "#6495ED"
-            onPress = {() => this.props.navigation.navigate('My')}
-          />
-          <Button
-            title = "Back!"
-            color = "gray"
-            onPress = {() => this.props.navigation.goBack()}
-          />
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
       </View>
     );
   }
 }
-
 
 class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state;
+    
+    return {
+      title: params ? params.otherParam : 'A Nested Details Screen',
+    }
+  };
+  
   render() {
+    /* 2. Read the params from the navigation state */
+    const { params } = this.props.navigation.state;
+    const itemId = params ? params.itemId : null;
+    const otherParam = params ? params.otherParam : null;
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFF8DC' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
-          <Button
-            title = "Go to Home"
-            onPress = {() => this.props.navigation.navigate('Home')}
-          />
-          <Button
-            title = "Go to Mine 😁"
-            color = "#6495ED"
-            onPress = {() => this.props.navigation.navigate('My')}
-          />
-          <Button
-            title = "Back!"
-            color = "gray"
-            onPress = {() => this.props.navigation.goBack()}
-          />
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+        <Button
+        title="Update the title"
+        onPress={() => this.props.navigation.setParams({otherParam: 'Updated!'})}
+        />
       </View>
     );
   }
 }
 
-
-class MyScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F0FFFF'}}>
-        <Text>My Beautiful Screen 😁</Text>
-          <Button
-            title = "Go to Home!"
-            onPress = {() => this.props.navigation.navigate('Home')}
-          />
-          <Button
-            title = "Go to Details"
-            color = "#6495ED"
-            onPress = {() => this.props.navigation.navigate('Details')}
-          />
-          <Button
-            title = "Back!"
-            color = "gray"
-            onPress = {() => this.props.navigation.goBack()}
-          />
-      </View>
-    );
-  }
-}
 
 const RootStack = StackNavigator(
   {
@@ -99,9 +100,6 @@ const RootStack = StackNavigator(
       screen: DetailsScreen,
     },
 
-    My: {
-      screen: MyScreen,
-    },
   },
 
   {
