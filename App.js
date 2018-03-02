e25f612b1390e855241bf358c3e20a5c00a93d31
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-02 14:59:57 +0330

    Some minor changes to learn flex

--- App.js
+++ App.js
@@ -1,58 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// AdCalc by MohamadKh75
+// 2018-03-01
+// ********************
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, AppRegistry } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import styles from './src/styles.js';
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class AdCalc extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+
+            <View style={styles.bala}>
+              <Text style={styles.balaText}>Here is bala :|</Text>
+            </View>
+
+            <View style={styles.paiin}>
+              <Text style={styles.paiinText}>Here is paiin :|:|</Text>
+            </View>
+
+        </View>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
