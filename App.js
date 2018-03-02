ad18b63ff86178ae506508a06ce2dba2cad9e6f3
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-02 20:14:19 +0330

    Add Navigation
    Use react-navigation

--- App.js
+++ App.js
@@ -3,26 +3,108 @@
 // ********************
 
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
 
 import styles from './src/styles.js';
 
 
-export default class AdCalc extends Component {
+export default class AdCalc extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-
-            <View style={styles.bala}>
-              <Text style={styles.balaText}>Here is balaaa :|</Text>
-            </View>
-
-            <View style={styles.paiin}>
-              <Text style={styles.paiinText}>Here is paiin :|:|</Text>
-            </View>
-
-        </View>
-
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+          <Button
+            title = "Go to Details"
+            onPress = {() => this.props.navigation.navigate('Details')}
+          />
+          <Button
+            title = "Go to My Page 😁"
+            color = "#6495ED"
+            onPress = {() => this.props.navigation.navigate('My')}
+          />
+          <Button
+            title = "Back!"
+            color = "gray"
+            onPress = {() => this.props.navigation.goBack()}
+          />
+      </View>
     );
   }
 }
+
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFF8DC' }}>
+        <Text>Details Screen</Text>
+          <Button
+            title = "Go to Home"
+            onPress = {() => this.props.navigation.navigate('Home')}
+          />
+          <Button
+            title = "Go to Mine 😁"
+            color = "#6495ED"
+            onPress = {() => this.props.navigation.navigate('My')}
+          />
+          <Button
+            title = "Back!"
+            color = "gray"
+            onPress = {() => this.props.navigation.goBack()}
+          />
+      </View>
+    );
+  }
+}
+
+
+class MyScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F0FFFF'}}>
+        <Text>My Beautiful Screen 😁</Text>
+          <Button
+            title = "Go to Home!"
+            onPress = {() => this.props.navigation.navigate('Home')}
+          />
+          <Button
+            title = "Go to Details"
+            color = "#6495ED"
+            onPress = {() => this.props.navigation.navigate('Details')}
+          />
+          <Button
+            title = "Back!"
+            color = "gray"
+            onPress = {() => this.props.navigation.goBack()}
+          />
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+
+    Details: {
+      screen: DetailsScreen,
+    },
+
+    My: {
+      screen: MyScreen,
+    },
+  },
+
+  {
+    initialRouteName: 'Home',
+  }
+);
