3b9b368a9d40c5dbf3a5fed5078ca18f4ec239cf
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-08 14:54:19 +0330

    Updated

--- src/styles.js
+++ src/styles.js
@@ -3,38 +3,46 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      alignItems: 'center',
       justifyContent: 'center',
-      alignItems: 'stretch',
-      backgroundColor: 'yellow',
-    },
-  
-    bala: {
-      flex: 1,
-      margin: 5,
-      backgroundColor: '#193441',
-      justifyContent: 'center',
-      alignItems: 'center'
+      backgroundColor: '#F0FFFF',
     },
 
-    balaText: {
-        fontSize: 25,
-        color: 'white',
-        textAlign: 'center',
-      },
-  
-    paiin: {
-      flex: 9,
-      margin: 5,
-      backgroundColor: '#3E606F',
-      justifyContent: 'center',
-      alignItems: 'center'
+    loginPageTop: {
+      flex: 2,
+      alignItems: 'center',
+      justifyContent: 'center'
     },
 
-    paiinText: {
-        fontSize: 25,
-        color: 'white',
-        textAlign: 'center',
-      },
+    loginPageLogo: {
+      width: 150,
+      height: 60,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    
+    loginPageMiddle: {
+      flex: 4,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    loginPageBottom: {
+      flex: 4,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    input: {
+      width: 120,
+      height: 50,
+      margin: 10,
+      padding: 3,
+      fontSize: 18,
+      borderWidth: 1,
+      borderColor: '#48bbec'
+    },
+
   });
 
 export default styles;
