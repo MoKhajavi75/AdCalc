7af74926307fabe6a226ef21f22f695b1dc5d9c8
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-08 23:36:33 +0330

    Editing Main

--- App.js
+++ App.js
@@ -14,110 +14,15 @@ import {Button,
         } from 'react-native';
 import { StackNavigator } from 'react-navigation'
 
-import styles from './src/styles.js';
-
 
 export default class AdCalc extends React.Component {
-  constructor(){
-    super();
-    
-    this.state = {
-        email: "",
-        fname: "",
-        lname: "",
-        password: "",
-        password_confirmation: "",
-    }
-}
-
-onRegisterPressed() {
-      
-}
-
-render() {
-    return (
-        <KeyboardAvoidingView style = {styles.container} behavior = 'padding'>
-
-          <View
-          style = {styles.loginPageTop}
-          >
-            <Image
-              source = {require('./src/img/logo_top.png')}
-              style = {styles.loginPageLogo}
-            />
-          </View>
-
-          <View style = {styles.loginPageMiddle} >
-            <TextInput
-              style = {styles.input}
-              placeholder = 'Email'
-              underlineColorAndroid = 'transparent' 
-              onChangeText =  {(text) => this.setState({email: text})}
-            />
-            <TextInput
-              style = {styles.input}
-              placeholder = "First Name"
-              underlineColorAndroid = 'transparent' 
-              onChangeText = {(text) => this.setState({fname: text})}
-            />
-            <TextInput
-              style = {styles.input}
-              placeholder = "Last Name"
-              underlineColorAndroid = 'transparent' 
-              onChangeText = {(text) => this.setState({lname: text})}
-            />
-            <TextInput
-              style = {styles.input}
-              placeholder = "Password"
-              secureTextEntry = {true}
-              underlineColorAndroid = 'transparent' 
-              onChangeText = {(text) => this.setState({password: text})}
-            />
-            <TextInput
-              style = {styles.input}
-              placeholder = "Password Confirmation"
-              underlineColorAndroid = 'transparent' 
-              secureTextEntry = {true}
-              onChangeText = {(text) => this.setState({password_confirmation: text})}
-            />
-          </View>
-          
-          <KeyboardAvoidingView style = {styles.loginPageBottom} behavior = 'position'>
-            <TouchableHighlight onPress={this.onRegisterPressed.bind(this)} style={styles.button}>
-              <Text style={styles.buttonText}>
-                Register!
-              </Text>
-            </TouchableHighlight>
-          </KeyboardAvoidingView>
-
-        </KeyboardAvoidingView>
-)
-}
-}
-
-/*
-class LogoTitle extends React.Component {
   render() {
-    return (
-      <Image
-        source={require('./src/img/logo_top.png')}
-        style={{ width: 100, height: 50, marginTop: 10, marginBottom: 10, alignItems: 'center' }}
-      />
-    );
+    return <RootStack />;
   }
 }
 
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: <LogoTitle />,
-  };
 
-  render() {
-    return (
-    );
-  }
-}
 
 class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -129,7 +34,7 @@ class DetailsScreen extends React.Component {
   };
   
   render() {
-    // 2. Read the params from the navigation state
+    /* 2. Read the params from the navigation state */
     const { params } = this.props.navigation.state;
     const itemId = params ? params.itemId : null;
     const otherParam = params ? params.otherParam : null;
@@ -163,7 +68,7 @@ class DetailsScreen extends React.Component {
 const RootStack = StackNavigator(
   {
     Home: {
-      screen: HomeScreen,
+      screen: Homess.HomeScreen,
     },
 
     Details: {
@@ -176,4 +81,6 @@ const RootStack = StackNavigator(
     initialRouteName: 'Home',
   }
 );
-*/
+
+
+AppRegistry.registerComponent('AdCalc', () => App);
