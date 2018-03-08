00ba3af860ba9663f6354c330ed9221d08810fc0
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-08 15:58:07 +0330

    Stretch Button
    You can achieve this by setting a flex:1 property on the parent element of the X, then assigning a flexDirection:row property to the X element.

--- src/styles.js
+++ src/styles.js
@@ -3,13 +3,12 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: '#F0FFFF',
     },
 
     loginPageTop: {
-      flex: 2,
+      flex: 1.5,
       alignItems: 'center',
       justifyContent: 'center'
     },
@@ -22,26 +21,40 @@ const styles = StyleSheet.create({
     },
     
     loginPageMiddle: {
-      flex: 4,
-      alignItems: 'center',
-      justifyContent: 'center'
+      flex: 5,
+      justifyContent: 'center',
     },
 
     loginPageBottom: {
-      flex: 4,
-      alignItems: 'center',
+      flex: 3.5,
       justifyContent: 'center'
     },
 
     input: {
-      width: 120,
+      alignSelf: 'stretch',
       height: 50,
       margin: 10,
+      marginHorizontal: 15,
       padding: 3,
       fontSize: 18,
       borderWidth: 1,
       borderColor: '#48bbec'
     },
+
+    button: {
+      height: 50,
+      marginHorizontal: 13,
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      backgroundColor: '#48BBEC',
+    },
+
+    buttonText: {
+      fontSize: 22,
+      color: '#FFF',
+      alignSelf: 'center'
+    },
+  
 
   });
 
