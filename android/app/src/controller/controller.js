316f4d7e3abd927ce16686370822748f082a5a51
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-10 22:26:12 +0330

    Update controllers
    regular-expressions.info
    
    yon.ir/owTjp

--- android/app/src/controller/controller.js
+++ android/app/src/controller/controller.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 
 validateEmail = (email) => {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
   };
+
+
+validateName = (name) => {
+  var regex = /^[a-zA-Z ]{2,30}$/;
+  return regex.test(name);
+}
+
+
+validatePassword = (pass) => {
+  var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  return regex.test(pass);
+}
 
 export default validateEmail;
