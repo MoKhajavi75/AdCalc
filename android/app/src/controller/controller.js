7aa0237905f63ff46f03a18f2826f3c88d01a2d5
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-11 11:57:36 +0330

    Update controllers

--- android/app/src/controller/controller.js
+++ android/app/src/controller/controller.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 
 
 validateEmail = (email) => {
+  if (email == "") {
+    alert("Please enter your Email address!")
+    return false;
+  }
+
   var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
   if (regex.test(email)) {
     return true;
   }
-
   else {
     alert("The Email address is invalid!")
+    return false;
   }
 };
 
 
 validateName = (name, isFirstName) => {
+  if (name == "") {
+    if (isFirstName) {
+      alert("Please enter your First Name!")
+    }
+    else {
+      alert("Please enter your Last Name!")
+    }
+    
+    return false;
+  }
+  
   var regex = /^[a-zA-Z ]{2,30}$/;
   
   if (regex.test(name)) {
@@ -25,23 +41,38 @@ validateName = (name, isFirstName) => {
     if (isFirstName) {
       alert("The First Name is invalid!")
     }
-
     else {
       alert("The Last Name is invalid!")
     }
+
+    return false;
   }
 }
 
 
-validatePassword = (pass) => {
-  var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  
-  if (regex.test(pass)) {
-    return true;
+validatePassword = (pass, isLogin = true) => {
+  if (isLogin) {
+    if (pass == "") {
+      alert("Please enter your Password!")
+      return false;
+    }
   }
 
   else {
-    alert("The Password must be:\n8 alphanumeric characters\nincluding one uppercase letter\nincluding one special character")
+    if (pass == "") {
+      alert("Please enter your Password!")
+      return false;
+    }
+    
+    var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    
+    if (regex.test(pass)) {
+      return true;
+    }
+    else {
+      alert("The Password must be:\n8 alphanumeric characters\nincluding one uppercase letter\nincluding one special character")
+      return false;
+    }
   }
 }
 
@@ -50,9 +81,9 @@ validateConfirm = (pass, passConf) => {
   if (pass == passConf) {
     return true;
   }
-
   else {
     alert("The Password does not match the confirm password!")
+    return false;
   }
 }
 
