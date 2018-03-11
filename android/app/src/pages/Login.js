346a6d5eff587e8aed7372bb74ad90736cbecc68
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-11 15:43:52 +0330

    Login Header is Transparent
    https://reactnavigation.org/docs/stack-navigator.html#navigationoptions-used-by-stacknavigator

--- android/app/src/pages/Login.js
+++ android/app/src/pages/Login.js
@@ -25,7 +25,10 @@ export default class Login extends React.Component {
   }
 
 
-  static navigationOptions = ({ navigation }) => {};
+  static navigationOptions = {
+    headerTransparent: true,
+  };
+
 
 
   onLoginPressed() {
