2dd0407e5044c2072472c3f8fc0f5aa5d83dd747
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-22 22:23:27 +0430

    Add YellowBox ignoreWarnings

--- App.js
+++ App.js
@@ -7,7 +7,15 @@ import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 import RootStack from './android/app/src/pages/Routes.js';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { YellowBox } from 'react-native';
 
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'NetInfo\'s "change" event is deprecated',
+]);
+  
 
 EStyleSheet.build({});
 
