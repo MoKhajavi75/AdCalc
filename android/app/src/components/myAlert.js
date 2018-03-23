885f42b69db6b00e0f0fc6eeee96bc6a99885319
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-23 17:51:52 +0430

    Fixed myAlert

--- android/app/src/components/myAlert.js
+++ android/app/src/components/myAlert.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import AwesomeAlert from 'react-native-awesome-alerts';
 
 
-export default function myAlert (showAlert, title) {
+
+export default function myAlert({ showAlert, _title, _msg, onDismiss }) {
   return <AwesomeAlert
-              show = {showAlert}
-              showProgress = {false}
-              title = "AwesomeAlert"
-              message = "I have a message for you!"
-              closeOnTouchOutside = {false}
-              closeOnHardwareBackPress = {false}
-              showCancelButton = {true}
-              showConfirmButton = {true}
-              cancelText = "No, cancel"
-              confirmText = "Yes, delete it"
-              confirmButtonColor = "#DD6B55"
+    show = {showAlert}
+    showProgress = {false}
 
-              onCancelPressed = {() => {
-                this.hideAlert();
-              }}
-              onConfirmPressed = {() => {
-                this.hideAlert();
-              }}
-            />
+    closeOnTouchOutside = {false}
+    closeOnHardwareBackPress = {false}
 
-  }
-
+    title = {_title}
+    message = {_msg}
+    
+    showConfirmButton = {true}
+    confirmButtonColor = "#DD6B55"
+    onConfirmPressed = {onDismiss}
+    confirmText = "Okay!"
+    
+    showCancelButton = {true}
+    onCancelPressed = {onDismiss}
+    cancelText = "Cancel"
+    
+    onDismiss = {onDismiss}
+  />
+}
 
 
 
 const styles = EStyleSheet.create({
-    errorText: {
-      fontSize: 14,
-      color: '#1DBC5C',
-      alignSelf: 'center',
-    }
-  });
+  errorText: {
+    fontSize: 14,
+    color: '#1DBC5C',
+    alignSelf: 'center',
+  }
+});
