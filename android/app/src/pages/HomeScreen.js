90755d2766a6910e0da0239c1762487bf6f92d3f
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-24 00:34:47 +0430

    Cleanups!

--- android/app/src/pages/HomeScreen.js
+++ android/app/src/pages/HomeScreen.js
@@ -1,39 +1,72 @@
 import {
-    StyleSheet,
-    Button,
-    Text,
-    View,
-    Image,
-    TextInput,
-    TouchableHighlight,
-    KeyboardAvoidingView,
-    } from 'react-native';
+  View,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  KeyboardAvoidingView,
+  NetInfo,
+  StatusBar,
+} from 'react-native';
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+} from 'native-base';
 
 
-class LogoTitle extends React.Component {
-    render() {
-      return (
-        <Image
-          source={require('../img/logo_top.png')}
-          style={{ width: 100, height: 50, marginTop: 10, marginBottom: 10, alignItems: 'center' }}
-        />
-      );
-    }
-  }
-
-  
 export default class HomeScreen extends Component {
-    static navigationOptions = {
-      headerTitle: <LogoTitle />,
-    };
-  
+  // navigation Options
+  static navigationOptions = {
+    header: null,
+    tabBarVisible: false,
+  };
+
+    // main render func
     render() {
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Home Screen</Text>
-        </View>
+        <Header
+        style = {{ backgroundColor: '#F8F8FF'}}
+        androidStatusBarColor="#2C3E50">
+
+        <Left style={{ flex: 1 }}>
+        </Left>
+
+        <Body style={{ flex: 1,  justifyContent: 'center', alignItems: 'center' }}>
+          <Image
+            source = {require('../img/logo_top.png')}
+            style = {styles.loginPageLogo}
+          />
+        </Body>
+
+        <Right style={{ flex: 1 }}>
+        </Right>
+      </Header>
       );
     }
   }
+
+
+
+// my lovely styles :D
+const styles = EStyleSheet.create({
+  loginPageLogo: {
+    width: 150,
+    height: 60,
+    margin: 15,
+    marginVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
