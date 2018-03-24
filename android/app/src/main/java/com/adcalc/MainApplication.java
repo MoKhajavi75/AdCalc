abe58dbd65907a10be31ad9bd3184302f07f8dc4
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-03-25 02:36:42 +0430

    Revert "Add react-native-splash-screen"
    This reverts commit 6c9fad1768a4b11e4a334bfa173668f33906b49e.

--- android/app/src/main/java/com/adcalc/MainApplication.java
+++ android/app/src/main/java/com/adcalc/MainApplication.java
@@ -3,7 +3,6 @@ package com.adcalc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenReactPackage()
+          new MainReactPackage()
       );
     }
 
