3404e8005bb7531ded455564a8fbca7b4bd500dd
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-04-01 20:57:48 +0430

    Now it's Pretty :grin:

--- android/app/src/components/myAlert.js
+++ android/app/src/components/myAlert.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import AwesomeAlert from 'react-native-awesome-alerts';
-
-
+import React from "react";
+import EStyleSheet from "react-native-extended-stylesheet";
+import AwesomeAlert from "react-native-awesome-alerts";
 
 export default function myAlert({ showAlert, _title, msg, onDismiss }) {
-  return <AwesomeAlert
-    show = {showAlert}
-    showProgress = {false}
-
-    closeOnTouchOutside = {false}
-    closeOnHardwareBackPress = {false}
-
-    title = {_title}
-    message = {msg}
-    
-    showConfirmButton = {true}
-    confirmButtonColor = "#1E90FFB0"
-    onConfirmPressed = {onDismiss}
-    confirmText = "Okay!"
-    
-    showCancelButton = {true}
-    cancelButtonColor = "#DD6B55"
-    onCancelPressed = {onDismiss}
-    cancelText = "Cancel"
-    
-    onDismiss = {onDismiss}
-  />
+  return (
+    <AwesomeAlert
+      show={showAlert}
+      showProgress={false}
+      closeOnTouchOutside={false}
+      closeOnHardwareBackPress={false}
+      title={_title}
+      message={msg}
+      showConfirmButton={true}
+      confirmButtonColor="#1E90FFB0"
+      onConfirmPressed={onDismiss}
+      confirmText="Okay!"
+      showCancelButton={true}
+      cancelButtonColor="#DD6B55"
+      onCancelPressed={onDismiss}
+      cancelText="Cancel"
+      onDismiss={onDismiss}
+    />
+  );
 }
